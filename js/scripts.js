75fe9f6b44bc6f3a9435d101ce6612b5ffc01262
lpost45 <lpost45@live.com>
2024-02-18 18:14:51 -0600

    Delete old code and add pokemon list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello Worl');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "Bulbasaur", height: 2.04, types: ['grass', 'poison'] },
+    { name: "Charmander", height: 2, types: 'fire' },
+    { name: "Squirtle", height: 1.08, types: 'water' }
+];
+console.log(pokemonList);
