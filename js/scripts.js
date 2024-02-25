7d14a793930f18e4d7de159661231a2c2651ee72
lpost45 <lpost45@live.com>
2024-02-25 14:21:30 -0700

    Reformatted quotations

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@ let pokemonList = [
     { 
         name: "Bulbasaur", 
         height: 2.04, 
-        types: ['grass', 'poison'] 
+        types: ["grass", "poison"] 
     },
     { 
         name: "Charmander", 
         height: 2, 
-        types: 'fire' 
+        types: "fire" 
     },
     { 
         name: "Squirtle", 
         height: 1.08, 
-        types: 'water' 
+        types: "water" 
     }
 ];
 document.write(pokemonList)
