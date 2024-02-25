a5435507982cfdc19fce687d54d022b2b3f4cd4a
lpost45 <lpost45@live.com>
2024-02-25 14:13:15 -0700

    Reformatted array, added loop for pokemon list with condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 let pokemonList = [
-    { name: "Bulbasaur", height: 2.04, types: ['grass', 'poison'] },
-    { name: "Charmander", height: 2, types: 'fire' },
-    { name: "Squirtle", height: 1.08, types: 'water' }
+    { 
+        name: "Bulbasaur", 
+        height: 2.04, 
+        types: ['grass', 'poison'] 
+    },
+    { 
+        name: "Charmander", 
+        height: 2, 
+        types: 'fire' 
+    },
+    { 
+        name: "Squirtle", 
+        height: 1.08, 
+        types: 'water' 
+    }
 ];
-console.log(pokemonList);
+document.write(pokemonList)
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height >2){
+        document.write(pokemonList[i] + "- Wow that's big!")
+    }
+}
