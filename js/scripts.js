e9132af3723b1e373a464dfdd04921b9ea7e2839
lpost45 <lpost45@live.com>
2024-02-27 22:12:02 -0700

    added more to loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
 let pokemonList = [
     { 
         name: "Bulbasaur", 
-        height: 2.04, 
+        height: (2.04), 
         types: ["grass", "poison"] 
     },
     { 
         name: "Charmander", 
-        height: 2, 
+        height: (2), 
         types: "fire" 
     },
     { 
         name: "Squirtle", 
-        height: 1.08, 
+        height: (1.08), 
         types: "water" 
     }
 ];
-document.write(pokemonList)
 for (let i=0; i < pokemonList.length; i++){
     if (pokemonList[i].height >2){
-        document.write(pokemonList[i] + "- Wow that's big!")
+        document.write(pokemonList[i].name + pokemonList[i].height + " - Wow that's big!")
+    }else if (pokemonList[i].height >1){
+        document.write(pokemonList[i].name + pokemonList[i].height)
     }
 }
