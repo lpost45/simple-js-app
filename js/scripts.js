e0c89eaaed1ed4c218839869c3f4d084c6976e9b
lpost45 <lpost45@live.com>
2024-03-04 20:52:12 -0700

    commented out old for loop, added forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,14 @@ let pokemonList = [
         types: "water" 
     }
 ];
-for (let i=0; i < pokemonList.length; i++){
-    if (pokemonList[i].height >2){
-        document.write("<p>" + pokemonList[i].name + pokemonList[i].height + " - Wow that's big!" + "</p>")
-    }else if (pokemonList[i].height >1){
-        document.write("<p>" + pokemonList[i].name + pokemonList[i].height + "</p>")
-    }
+// for (let i=0; i < pokemonList.length; i++){
+   // if (pokemonList[i].height >2){
+      //  document.write("<p>" + pokemonList[i].name + pokemonList[i].height + " - Wow that's big!" + "</p>")
+    //}else if (pokemonList[i].height >0){
+       // document.write("<p>" + pokemonList[i].name + pokemonList[i].height + "</p>")
+    //}
+//}
+function loopFunction(pokemon) {
+    document.write("<p>" + pokemon.name + " " + pokemon.height + "</p>");
 }
+pokemonList.forEach(loopFunction);
