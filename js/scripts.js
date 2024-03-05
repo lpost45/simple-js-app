883c0d5a920c5904f5ba76fdc540ad3a551e747e
lpost45 <lpost45@live.com>
2024-03-04 21:52:54 -0700

    Wrapped array in IIEF, added add function, added getAll function, added document.write for array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,34 @@
-let pokemonList = [
-    { 
-        name: "Bulbasaur", 
-        height: (2.04), 
-        types: ["grass", "poison"] 
-    },
-    { 
-        name: "Charmander", 
-        height: (2), 
-        types: "fire" 
-    },
-    { 
-        name: "Squirtle", 
-        height: (1.08), 
-        types: "water" 
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { 
+            name: "Bulbasaur", 
+            height: (2.04), 
+            types: ["grass", "poison"] 
+        },
+        { 
+            name: "Charmander", 
+            height: (2), 
+            types: "fire" 
+        },
+        { 
+            name: "Squirtle", 
+            height: (1.08), 
+            types: "water" 
+        }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
 // for (let i=0; i < pokemonList.length; i++){
    // if (pokemonList[i].height >2){
       //  document.write("<p>" + pokemonList[i].name + pokemonList[i].height + " - Wow that's big!" + "</p>")
@@ -22,7 +36,8 @@ let pokemonList = [
        // document.write("<p>" + pokemonList[i].name + pokemonList[i].height + "</p>")
     //}
 //}
-function loopFunction(pokemon) {
-    document.write("<p>" + pokemon.name + " " + pokemon.height + "</p>");
-}
-pokemonList.forEach(loopFunction);
+//function loopFunction(pokemon) {
+//    document.write("<p>" + pokemon.name + " " + pokemon.height + "</p>");
+//}
+//pokemonList.forEach(loopFunction);
+document.write(pokemonRepository.getAll())
